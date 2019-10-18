3e9d055533e8bd0ed0ea3f39d3478f7ae0789a62
Bram Mittendorff <bram.mittendorff@dealerdirect.nl>
2019-10-18 13:39:48 +0200

    Update db.py it contains db creds
    Update db.py it contains db creds

--- db.py
+++ db.py
@@ -1,12 +1,8 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 
-
-# engine = create_engine('mysql+pymysql://root:chigua818@112.124.201.185/'
-#                        'onlooker?charset=utf8mb4')
-
-engine = create_engine('mysql+pymysql://root:mysql@47.110.76.95/'
-                       'nba?charset=utf8mb4')
+engine = create_engine(os.getenv("DATABASE_URL"))
 
 DBSession = sessionmaker(bind=engine)
 session = scoped_session(DBSession)
